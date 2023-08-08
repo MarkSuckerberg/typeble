7ae838cfc1b6233588eb3dbd7e01f6dbd73dc4ba
Mark Suckerberg <mark@suckerberg.gay>
2023-08-08 13:52:37 -0500

    Actually adds the new functions to index

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,7 @@
 export * from "./AccessTumblrApi";
 export * from "./Block";
 export * from "./BlogInfo";
+export * from "./FilterBlocks";
 export * from "./Follow";
 export * from "./Like";
 export * from "./Post";
