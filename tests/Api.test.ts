85c44bf79bae2e48c611e25724650771cb0f2f7e
Mark Suckerberg <mark@suckerberg.gay>
2024-03-11 22:08:17 -0500

    oops

--- tests/Api.test.ts
+++ tests/Api.test.ts
@@ -5,6 +5,6 @@ const token = process.env.TUMBLR_TOKEN;
 if (!token) throw new Error("No token provided");
 
 it("should error on invalid path", async () => {
-	const error = await accessTumblrAPI(token, "invalid/path").catch<Error>(e => e);
-	expect(error instanceof Error).toBe(true);
+	const response = await accessTumblrAPI(token, "invalid/path");
+	expect(response.meta.status).toBe(404);
 });
